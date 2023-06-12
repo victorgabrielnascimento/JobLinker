7f49c1a4b46c4137cf53cbf890843054eab5a1e4
Gabrielyn Strujak <107216619+strujakgabrielyn@users.noreply.github.com>
2023-06-11 22:53:35 -0300

    cadastro

--- joblinker-frontend/src/components/Cadastro.jsx
+++ joblinker-frontend/src/components/Cadastro.jsx
@@ -2,7 +2,6 @@ function Cadastro() {
     return (
         <div className="match">
             <div className="login">
-                <h1>Bem vindo</h1>
                 <h2>Cadastre-se</h2>
                 <div className="inputs-login">
                     <input type="text" name="nome" id="nome" placeholder="Nome"/>
@@ -10,6 +9,14 @@ function Cadastro() {
                     <input type="text" name="email" id="email" placeholder="EMAIL"/>
                     <input type="password" name="senha" id="senha" placeholder="SENHA" />
                     <input type="password" name="confirmar-senha" id="confirmar-senha" placeholder="CONFIRME A SENHA" />
+                    <div className="endereco">
+                        <input type="text" className="w-50" name="rua" id="rua" placeholder="Rua"/>
+                        <input type="text" name="numero" id="numero" placeholder="Número"/>
+                        <input type="text" name="complemento" id="complemento" placeholder="Complemento"/>
+                        <input type="text" className="w-50"  name="cidade" id="cidade" placeholder="Cidade"/>
+                        <input type="text" name="estado" id="estado" placeholder="Estado"/>
+                        <input type="text" name="cep" id="cep" placeholder="CEP"/>
+                    </div>
                 </div>
                 <div className="btns-login">
                     <button className="btn-login">Registrar</button>
